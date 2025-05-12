7ea9c79233dceb0b0f03ffa5aeaf2f2c1f114268
GuilhermeHorta22 <ghorta800@gmail.com>
2025-05-11 23:14:20 -0300

    UserModel e UserDal feito

--- src/config/DataBase.ts
+++ src/config/DataBase.ts
@@ -1,21 +1,24 @@
 import dotenv from 'dotenv'; 
-dotenv.config();  // Carrega as variáveis do arquivo .env
+dotenv.config();  //carrega as variáveis do arquivo .env
 
 import { Pool } from 'pg';
 
-// Aqui, usamos a conexão via variáveis de ambiente
-export class DataBase {
+//usamos a conexão via variáveis de ambiente
+export class DataBase 
+{
   private static instance: Pool;
 
-  // O método getInstance retorna a instância do Pool
-  public static getInstance(): Pool {
-    if (!DataBase.instance) {
-      DataBase.instance = new Pool({
-        user: process.env.PG_USER,          // Usuário do PostgreSQL
-        host: process.env.PG_HOST,          // Host do banco (ex: localhost)
-        database: process.env.PG_DATABASE,  // Nome do banco de dados
-        password: process.env.PG_PASSWORD,  // Senha do usuário
-        port: Number(process.env.PG_PORT),  // Porta do PostgreSQL
+  //método getInstance retorna a instância do Pool
+  public static getInstance(): Pool 
+  {
+    if(!DataBase.instance) 
+    {
+        DataBase.instance = new Pool({
+          user: process.env.PG_USER,         
+          host: process.env.PG_HOST,          
+          database: process.env.PG_DATABASE,  
+          password: process.env.PG_PASSWORD,  
+          port: Number(process.env.PG_PORT),
       });
     }
     return DataBase.instance;
--- src/dals/UserDal.ts
+++ src/dals/UserDal.ts
@@ -2,43 +2,46 @@ import { Pool } from 'pg';
 import { UserModel } from '../models/UserModel';
 import { DataBase } from '../config/DataBase'; // Singleton de conexão
 
-export class UserDAL {
+export class UserDAL 
+{
   private db: Pool;
 
-  constructor() {
+  constructor()
+  {
     this.db = DataBase.getInstance(); // Usa Singleton para o Pool
   }
 
-  async findByCPF(cpf: string): Promise<UserModel | null> {
-    const result = await this.db.query('SELECT * FROM credenciais WHERE cpf = $1', [cpf]);
-    if (result.rows.length === 0) return null;
-
-    const row = result.rows[0];
-    return new UserModel(row.id, row.cpf, row.senha);
+  //buscando por um usuario pelo cpf
+  async findByCPF(cpf: string): Promise<UserModel | null>
+  {
+    const result = await this.db.query('SELECT * FROM credenciais WHERE cre_login = $1', [cpf]);
+    if(result.rows.length != 0)
+    {
+        const row = result.rows[0];
+        return new UserModel(row.cre_login,row.cre_senha,row.cre_cod);
+    } 
+    return null;
   }
 
   async create(user: UserModel): Promise<UserModel>
   {
     const result = await this.db.query(
-        'INSERT INTO credenciais (cpf, senha) VALUES ($1, $2) RETURNING id',
+        'INSERT INTO credenciais (cre_login, cre_senha) VALUES ($1, $2) RETURNING cre_cod',
         [user.getCpf(), user.getSenha()]
     );
-    const insertedId = result.rows[0].id;
+    const insertedId = result.rows[0].cre_cod;
     return new UserModel(user.getCpf(), user.getSenha(), insertedId);
   }
 
   //update
   async updateSenha(cpf: string, novaSenha: string): Promise<void>
   {
-    await this.db.query(
-      'UPDATE users SET senha = $1 WHERE cpf = $2',
-      [novaSenha, cpf]
-    );
+    await this.db.query('UPDATE credenciais SET cre_senha = $1 WHERE cre_login = $2',[novaSenha, cpf]);
   }
 
   //delete
   async deleteByCPF(cpf: string): Promise<void>
   {
-    await this.db.query('DELETE FROM credenciais WHERE cpf = $1', [cpf]);
+    await this.db.query('DELETE FROM credenciais WHERE cre_login = $1', [cpf]);
   }
 }
