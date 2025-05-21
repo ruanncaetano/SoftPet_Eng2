3500a7b1861f21f297f906eb693283cb42b0e47a
luan <luanpds10@gmail.com>
2025-05-21 18:00:34 -0300

    Criação de id auto-incremento

--- BackEnd/src/main/java/SoftPet/backend/model/UserModel.java
+++ BackEnd/src/main/java/SoftPet/backend/model/UserModel.java
@@ -1,7 +1,13 @@
 package SoftPet.backend.model;
 
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
 public class UserModel
 {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String cpf;
     private String senha;
